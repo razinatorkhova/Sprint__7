44583b2b04a0f3166cfc2b71535b01bd5404e357
Razina Torkhova <razina.torkhova@gmail.com>
2025-02-09 22:47:49 +0100

    add model files

---
+++ src/main/java/ru/practikum/yandex/model/CourierDataLombok.java
@@ -0,0 +1,17 @@
+package ru.practikum.yandex.model;
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CourierDataLombok {
+    private String login;
+    private String password;
+    private String firstName;
+    }
+
+
